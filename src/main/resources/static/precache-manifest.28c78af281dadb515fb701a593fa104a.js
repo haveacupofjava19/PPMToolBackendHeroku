5cea6c3dabffb4f9e33f1a3bc1203033b8179a29
haveacupofjava19 <saketdeshmukh95@gmail.com>
2019-07-28 23:40:55 +0530

    react

---
+++ src/main/resources/static/precache-manifest.28c78af281dadb515fb701a593fa104a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb325d0cc077e773613d161f261b635a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500100bda4b2bbbd7647",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f21d38ac332c8c74bb17",
+    "url": "/static/css/main.71a63cf9.chunk.css"
+  },
+  {
+    "revision": "500100bda4b2bbbd7647",
+    "url": "/static/js/2.a807eb96.chunk.js"
+  },
+  {
+    "revision": "f21d38ac332c8c74bb17",
+    "url": "/static/js/main.e252f93d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
