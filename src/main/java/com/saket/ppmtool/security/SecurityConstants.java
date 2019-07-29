7bf0f72146c16daff54873080caf548e3bb252fe
haveacupofjava19 <saketdeshmukh95@gmail.com>
2019-07-30 00:04:31 +0530

    time extended

--- src/main/java/com/saket/ppmtool/security/SecurityConstants.java
+++ src/main/java/com/saket/ppmtool/security/SecurityConstants.java
@@ -10,6 +10,6 @@ public class SecurityConstants {
 
     public static final String HEADER_STRING = "Authorization";
 
-    public static final long EXPIRATION_TIME = 30_000;
+    public static final long EXPIRATION_TIME = 900000;
 
 }
